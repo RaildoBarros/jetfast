61afc9c4edbeb07148ac786b59e175d8540375f9
Raildo <raildo.barros@gmail.com>
2026-01-25 15:28:07 -0400

    config

--- configuration/production.py
+++ configuration/production.py
@@ -147,3 +147,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
